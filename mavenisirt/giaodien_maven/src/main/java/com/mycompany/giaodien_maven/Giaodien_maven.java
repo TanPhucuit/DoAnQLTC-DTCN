77de2a31c52541e6fd8aca50571880b3a0dbf28e
ynhiuit <23521108@gm.uit.edu.vn>
2025-05-01 21:41:40 +0700

    thay doi giao dien

---
+++ mavenisirt/giaodien_maven/src/main/java/com/mycompany/giaodien_maven/Giaodien_maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.giaodien_maven;
+
+/**
+ *
+ * @author Admin
+ */
+public class Giaodien_maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
